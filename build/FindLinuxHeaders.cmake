f1bfe63864ea6a199884103fec66c8956e633948
Jon0 <Jono4728@gmail.com>
2017-02-20 23:48:15 +1300

    includes

---
+++ build/FindLinuxHeaders.cmake
@@ -0,0 +1,27 @@
+
+# Find the kernel release
+execute_process(
+    COMMAND uname -r
+    OUTPUT_VARIABLE KERNEL_RELEASE
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+
+# Find the headers
+find_path(KERNELHEADERS_DIR
+    PATHS /lib/modules/linux-headers-${KERNEL_RELEASE}
+)
+
+
+if (KERNELHEADERS_DIR)
+    set(KERNELHEADERS_INCLUDE_DIRS
+        ${KERNELHEADERS_DIR}/include
+        ${KERNELHEADERS_DIR}/arch/x86/include
+        CACHE PATH "Kernel headers include dirs"
+    )
+    set(KERNELHEADERS_FOUND 1 CACHE STRING "Set to 1 if kernel headers were found")
+else (KERNELHEADERS_DIR)
+    set(KERNELHEADERS_FOUND 0 CACHE STRING "Set to 1 if kernel headers were found")
+endif (KERNELHEADERS_DIR)
+
+mark_as_advanced(KERNELHEADERS_FOUND)
